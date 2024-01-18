5b60d6e62ab987f1ed2934800b6fb9c4f2ee5d49
gameyp <yossatorn.pht@student.mahidol.edu>
2024-01-18 11:50:49 +0700

    handling 404
    - add function to handle 404
    - add page 404

--- app.py
+++ app.py
@@ -93,5 +93,10 @@ def addRequest():
     
     return redirect(url_for('login'))
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
